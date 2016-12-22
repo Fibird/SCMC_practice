b61ec9959b724afb79bc7aa0e6163454e9fb78be
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-23 00:42:26 +0800

    Refactor
    I move the ISR into other place because it's too long. And to improve the readability of the code, I change the name of label.

--- counterLab.asm
+++ counterLab.asm
@@ -4,24 +4,10 @@
 		pcon_add equ 0ff23h
 		
 		org 0000h
-		sjmp main
+		ljmp main
 
 		org 000bh
-		mov a,#10
-		subb a,r0
-		jz exit		;计数到10清零
-		mov a,r0		
-		mov dptr,#disdata
-		movc a,@a+dptr		  ;设置段码
-		mov dptr,#pa_add	
-		movx @dptr,a
-		
-		mov a,r0
-		inc a
-		mov r0,a
-		
-exit:	mov r0,#0
-		reti
+		ljmp isr_t0
 		
 main:	mov r0,#0
 		mov dptr,#pcon_add	  ;设置8255
@@ -32,15 +18,41 @@ main:	mov r0,#0
 		movx @dptr,a
 
 		mov ie,#82h		 ;允许定时器0中断
-
-		mov tmod,#10h	 ;计数器设置
-		mov th0,#00h
-		mov tl0,#00h
+		mov tmod,#01h	 ;计数器设置
+		mov th0,#0bh
+		mov tl0,#0cdh
 		setb tr0
+		mov 30h,08h
 
 here:	nop
 		nop
 		sjmp here
+
+isr_t0:	mov th0,#0bh
+		mov tl0,#0cdh
+		mov a,30h
+		dec a
+		mov 30h,a
+		jnz ret0
+
+check:	mov a,#10
+		subb a,r0
+		jz clear		;计数到10清零
+
+disp:	mov a,r0		
+		mov dptr,#disdata
+		movc a,@a+dptr		  ;设置段码
+		mov dptr,#pa_add	
+		movx @dptr,a
+		
+		mov a,r0
+		inc a
+		mov r0,a
+		
+ret0:	reti
+
+clear:	mov r0,#0
+		jmp disp
 
 disdata:
 		db 3fh,06h,5bh,4fh,66h,6dh,7dh,07h,7fh,90h
