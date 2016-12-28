42673ae53cac2a900a5d64bc6ef6ca8c96692d63
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-28 22:27:53 +0800

    correct the part of 8255 and LEDs

--- traffic_lights.asm
+++ traffic_lights.asm
@@ -11,10 +11,8 @@ main:	lcall clrreg
 		mov dptr,#pcon_add	  ;设置8255控制字
 		mov a,#81h
 		movx @dptr,a
-
-		;R0和R1表示南北的计时时间，R2和R3表示东西的计时时间
-		;需先向r4寄存器写入时间单位为秒
-here:	mov p1,#28h	;东西红灯亮，南北黄灯亮
+		;东西红灯亮，南北黄灯亮
+here:	mov p1,#28h	
 		mov r4,#time1
 loop0:	mov r5,#time0	
 loop1:	mov tmod,#01h	 ;计数器工作方式设置
@@ -35,8 +33,8 @@ check0:	cjne r2,#10,notclr
 		inc r7
 check1:	cjne r7,#2,notclr
 carry:	inc r3
-		clr p1.4	;南北黄灯灭
-		setb p1.5	;南北绿灯亮		
+		clr p1.4		 ;南北黄灯灭
+		setb p1.5		 ;南北绿灯亮		
 
 notclr:	call disp
 		inc r0
@@ -44,7 +42,7 @@ notclr:	call disp
 		djnz r4,loop0
 		call clrreg
 		nop
-		;R0和R1表示东西计时时间，R2和R3表示南北的计时时间
+		;南北红灯亮；东西黄灯亮
 		mov p1,#82h
 		mov r4,#time1
 loop2:	mov r5,#time0	
@@ -66,8 +64,8 @@ check2:	cjne r0,#10,notclr1
 		inc r7
 check3:	cjne r7,#2,notclr1
 carry1:	inc r1
-		clr p1.0	;东西黄灯灭
-		setb p1.1	;东西绿灯亮
+		clr p1.0		 ;东西黄灯灭
+		setb p1.1		 ;东西绿灯亮
 
 notclr1:call disp
 		inc r2
@@ -76,75 +74,66 @@ notclr1:call disp
 		call clrreg
 		sjmp here
 
-		;子程序disp，调用前需向R0和R1寄存器写值
-		;寄存器R0存放个位，寄存器R1存放十位
+		;子程序disp，从寄存器R0、R1、R2、R3中
+		;读取数码管的字形码
 disp:	mov dptr,#pb_add	;消隐
 		mov a,#0ffh
 		movx @dptr,a
 				
 		mov a,r1		
 		mov dptr,#disdata
-		movc a,@a+dptr		  ;查表设置段码
+		movc a,@a+dptr		 ;查表设置段码
 		mov dptr,#pb_add	
 		movx @dptr,a
 		mov dptr,#pa_add	 ;设置位码
-		mov a,#0cch			  
+		mov a,#0fdh			  
 		movx @dptr,a 
 		
-		mov dptr,#pb_add	;消隐
+		mov dptr,#pb_add	 ;消隐
 		mov a,#0ffh
-		movx @dptr,a
-		mov a,#00
-		mov dptr,#pa_add
 		movx @dptr,a
 
 		mov a,r0		
 		mov dptr,#disdata
-		movc a,@a+dptr		  ;查表设置段码
+		movc a,@a+dptr		 ;查表设置段码
 		mov dptr,#pb_add	
 		movx @dptr,a
 		mov dptr,#pa_add	 ;设置位码
-		mov a,#0cch			  
+		mov a,#0feh			  
 		movx @dptr,a 
 		
-		mov dptr,#pb_add	;消隐
+		mov dptr,#pb_add	 ;消隐
 		mov a,#0ffh
 		movx @dptr,a
-		mov a,#00
-		mov dptr,#pa_add
-		movx @dptr,a 
 				
 		mov a,r2		
 		mov dptr,#disdata
-		movc a,@a+dptr		  ;查表设置段码
+		movc a,@a+dptr		 ;查表设置段码
 		mov dptr,#pb_add	
 		movx @dptr,a
 		mov dptr,#pa_add	 ;设置位码
-		mov a,#0cch			  
+		mov a,#0dfh			  
 		movx @dptr,a 
 		
-		mov dptr,#pb_add	;消隐
+		mov dptr,#pb_add	 ;消隐
 		mov a,#0ffh
-		movx @dptr,a
-		mov a,#00
-		mov dptr,#pa_add
 		movx @dptr,a
 
 		mov a,r3		
 		mov dptr,#disdata
-		movc a,@a+dptr		  ;查表设置段码
+		movc a,@a+dptr		 ;查表设置段码
 		mov dptr,#pb_add	
 		movx @dptr,a
 		mov dptr,#pa_add	 ;设置位码
-		mov a,#0cch			  
+		mov a,#0efh			  
 		movx @dptr,a 		
 		ret
 
 		;清零寄存器
-clrreg:	mov r0,#0 	;东西个位
-		mov r1,#0	;东西十位
-		mov r2,#0	;南北个位
-		mov r3,#0	;南北十位
+clrreg:	mov r0,#0 			 ;东西个位
+		mov r1,#0			 ;东西十位
+		mov r2,#0			 ;南北个位
+		mov r3,#0			 ;南北十位
 		mov a,#0
 		mov r7,#0
 		mov p1,#0
