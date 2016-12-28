d96f7133f0c45e1c5d85dc8c4747a79f35355363
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-28 22:14:09 +0800

    The part of counter has been corrected

--- traffic_lights.asm
+++ traffic_lights.asm
@@ -12,10 +12,9 @@ main:	lcall clrreg
 		mov a,#81h
 		movx @dptr,a
 
-		mov ie,#00h		 ;关中断
-
-here:	;R0和R1表示南北的计时时间，R2和R3表示东西的计时时间
+		;R0和R1表示南北的计时时间，R2和R3表示东西的计时时间
 		;需先向r4寄存器写入时间单位为秒
+here:	mov p1,#28h	;东西红灯亮，南北黄灯亮
 		mov r4,#time1
 loop0:	mov r5,#time0	
 loop1:	mov tmod,#01h	 ;计数器工作方式设置
@@ -24,30 +23,29 @@ loop1:	mov tmod,#01h	 ;计数器工作方式设置
 		setb tr0
 again:	jnb tf0,again
 		clr tf0
-		;clr tr0
+		clr tr0
 		djnz r5,loop1
 
 		cjne r0,#10,check0 
-		mov r0,#0
-		setb p1.2	;东西红灯亮
+		mov r0,#0		
 		inc r1
-check0:	cjne r2,#10,check1
+check0:	cjne r2,#10,notclr
 		mov r2,#0
-		setb p1.4	;南北黄灯亮
+		cjne r3,#0,carry
 		inc r7
 check1:	cjne r7,#2,notclr
-		inc r3
-		setb p1.5	;南北绿灯亮
+carry:	inc r3
 		clr p1.4	;南北黄灯灭
-		mov r7,#2
+		setb p1.5	;南北绿灯亮		
 
-notclr:	lcall disp
+notclr:	call disp
 		inc r0
 		inc r2
 		djnz r4,loop0
-		lcall clrreg
+		call clrreg
 		nop
 		;R0和R1表示东西计时时间，R2和R3表示南北的计时时间
+		mov p1,#82h
 		mov r4,#time1
 loop2:	mov r5,#time0	
 loop3:	mov tmod,#01h	 ;计数器工作方式设置
@@ -56,28 +54,26 @@ loop3:	mov tmod,#01h	 ;计数器工作方式设置
 		setb tr0
 again1:	jnb tf0,again1
 		clr tf0
-		;clr tr0
+		clr tr0
 		djnz r5,loop3
 
 		cjne r2,#10,check2 
 		mov r2,#0
-		setb p1.6	;南北红灯亮
 		inc r3
-check2:	cjne r0,#10,check3
+check2:	cjne r0,#10,notclr1
 		mov r0,#0
-		setb p1.0	;东西黄灯亮
+		cjne r1,#0,carry1
 		inc r7
 check3:	cjne r7,#2,notclr1
-		inc r1
-		setb p1.1	;东西绿灯亮
+carry1:	inc r1
 		clr p1.0	;东西黄灯灭
-		mov r7,#2
+		setb p1.1	;东西绿灯亮
 
 notclr1:call disp
 		inc r2
 		inc r0
 		djnz r4,loop2
-		lcall clrreg
+		call clrreg
 		sjmp here
 
 		;子程序disp，调用前需向R0和R1寄存器写值
@@ -85,9 +81,6 @@ notclr1:call disp
 disp:	mov dptr,#pb_add	;消隐
 		mov a,#0ffh
 		movx @dptr,a
-		mov a,#00
-		mov dptr,#pa_add
-		movx @dptr,a 
 				
 		mov a,r1		
 		mov dptr,#disdata
