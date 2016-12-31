2c5e0ce9b1a86da7e2d6d8c69a297333bb92de6f
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-31 21:36:22 +0800

    Refactor this code
    I use loop to save a lot of lines.

--- traffic_lights1.asm
+++ traffic_lights1.asm
@@ -16,7 +16,9 @@ main:	mov r0,#0
 		mov dptr,#pcon_add		;初始化8255	 
 		mov a,#81h
 		movx @dptr,a
-		mov a,#6
+		
+		mov buf_add+2,#0ffh
+		mov buf_add+3,#0ffh
 
 		mov ie,#82h	
 		mov 30h,#int_times		;设置中断次数	
@@ -26,7 +28,7 @@ main:	mov r0,#0
 		setb tr0				;启动计数器		
 		clr f0					
 		mov p1,#0bdh			;初始情况下，东西红灯亮南北绿灯亮
-		mov r0,#60
+		mov r2,#60
 		mov r1,#50
 		
 here:	nop
@@ -43,7 +45,7 @@ isr_t0: mov th0,#0bh
 		
 		mov 30h,#int_times		;重置中断次数
 		jb f0,next
-		mov a,r0
+		mov a,r2
 		mov b,#10
 		div ab
 		mov buf_add+0,b
@@ -51,25 +53,25 @@ isr_t0: mov th0,#0bh
 		mov a,r1
 		mov b,#10
 		div ab
-		mov buf_add+5,b
-		mov buf_add+6,a
+		mov buf_add+4,b
+		mov buf_add+5,a
 
 		cjne r1,#0,check0	;判断绿灯或黄灯是否结束
 		mov r1,#10		;开始黄灯的10s
 		setb p1.1		;灭绿灯
 		clr p1.0		;亮黄灯
 
-check0:	cjne r0,#0,dec_f
-		mov r0,#60
+check0:	cjne r2,#0,dec_f
+		mov r2,#60
 		mov r1,#50
 		setb f0
 		mov p1,#0dbh	;南北红灯亮，东西绿灯亮
 		;进入下一个状态		
-next:	mov a,r0
+next:	mov a,r2
 		mov b,#10
 		div ab
-		mov buf_add+5,b
-		mov buf_add+6,a
+		mov buf_add+4,b
+		mov buf_add+5,a
 		mov a,r1
 		mov b,#10
 		div ab
@@ -81,31 +83,26 @@ next:	mov a,r0
 		setb p1.5		;灭绿灯
 		clr p1.4		;亮黄灯 
 
-check1:	cjne r0,#0,dec_f
-		mov r0,#60
+check1:	cjne r2,#0,dec_f
+		mov r2,#60
 		mov r1,#50
 		clr f0
 		mov p1,#0bdh	;南北红灯亮，东西绿灯亮
 		
-dec_f:	dec r0
+dec_f:	dec r2
 		dec r1
 
 ret0:	reti
 
-disp:	mov dptr,#pb_add	
-		mov a,#0ffh
-		movx @dptr,a
-		mov a,#0ffh
-		mov dptr,#pa_add
-		movx @dptr,a
-				
-		mov a,buf_add+1		
+disp:	mov r3,#0feh	  ;存放位码
+		mov r0,#buf_add	  ;存放段码的地址
+loop1:	mov a,@r0	
 		mov dptr,#disdata
 		movc a,@a+dptr		 
 		mov dptr,#pb_add	
 		movx @dptr,a
 		mov dptr,#pa_add	 
-		mov a,#0fdh			  
+		mov a,r3			  
 		movx @dptr,a 
 		
 		mov dptr,#pb_add	
@@ -115,47 +112,18 @@ disp:	mov dptr,#pb_add
 		mov dptr,#pa_add
 		movx @dptr,a
 
-		mov a,buf_add+0		
-		mov dptr,#disdata
-		movc a,@a+dptr	
-		mov dptr,#pb_add	
-		movx @dptr,a
-		mov dptr,#pa_add
-		mov a,#0feh			  
-		movx @dptr,a 
-		
-		mov dptr,#pb_add	
-		mov a,#0ffh
-		movx @dptr,a
-		mov a,#0ffh
-		mov dptr,#pa_add
-		movx @dptr,a
+delay:	mov r5,#0fh		 	;调用延时
+loop2:	nop
+		djnz r5,loop2
+
+		inc r0
+		mov a,r3 
+		jnb acc.5,ret1
+		rl a
+		mov r3,a
+		jmp loop1
 				
-		mov a,buf_add+6		
-		mov dptr,#disdata
-		movc a,@a+dptr		
-		mov dptr,#pb_add	
-		movx @dptr,a
-		mov dptr,#pa_add	 
-		mov a,#0dfh			  
-		movx @dptr,a 
-		
-		mov dptr,#pb_add	
-		mov a,#0ffh
-		movx @dptr,a
-		mov a,#0ffh
-		mov dptr,#pa_add
-		movx @dptr,a
-
-		mov a,buf_add+5		
-		mov dptr,#disdata
-		movc a,@a+dptr	
-		mov dptr,#pb_add	
-		movx @dptr,a
-		mov dptr,#pa_add	
-		mov a,#0efh			  
-		movx @dptr,a 		
-		ret
+ret1:	ret
 		
 disdata:
 		db 0c0h,0f9h,0a4h,0b0h,99h,92h,82h,0f8h,80h,90h,0c0h
